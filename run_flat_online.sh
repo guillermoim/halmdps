b7b41b87aaea52e29ea5571f33a4734014d72f93
Guillermo <guillermo.infante@upf.edu>
2024-01-15 16:58:05 +0100

    modified conf files

--- run_flat_online.sh
+++ run_flat_online.sh
@@ -5,7 +5,28 @@
 # python online_flat.py +experiment=f-nroom-v6 ++seed=100
 # python online_flat.py +experiment=f-nroom-v7 ++seed=100
 
-python online_flat.py +experiment=f-taxi-v0 ++seed=100
-python online_flat.py +experiment=f-taxi-v1 ++seed=100
-python online_flat.py +experiment=f-taxi-v2 ++seed=100
-python online_flat.py +experiment=f-taxi-v3 ++seed=100
+python online_flat.py +experiment=f-taxi-v0 ++seed=1001
+python online_flat.py +experiment=f-taxi-v0 ++seed=2222
+python online_flat.py +experiment=f-taxi-v0 ++seed=4444
+python online_flat.py +experiment=f-taxi-v0 ++seed=7189
+python online_flat.py +experiment=f-taxi-v0 ++seed=6301
+
+python online_flat.py +experiment=f-taxi-v1 ++seed=1001
+python online_flat.py +experiment=f-taxi-v1 ++seed=2222
+python online_flat.py +experiment=f-taxi-v1 ++seed=4444
+python online_flat.py +experiment=f-taxi-v1 ++seed=7189
+python online_flat.py +experiment=f-taxi-v1 ++seed=7301
+
+python online_flat.py +experiment=f-taxi-v2 ++seed=1001
+python online_flat.py +experiment=f-taxi-v2 ++seed=2222
+python online_flat.py +experiment=f-taxi-v2 ++seed=4444
+python online_flat.py +experiment=f-taxi-v2 ++seed=7189
+python online_flat.py +experiment=f-taxi-v2 ++seed=7301
+
+python online_flat.py +experiment=f-taxi-v3 ++seed=1001
+python online_flat.py +experiment=f-taxi-v3 ++seed=2222
+python online_flat.py +experiment=f-taxi-v3 ++seed=4444
+python online_flat.py +experiment=f-taxi-v3 ++seed=7189
+python online_flat.py +experiment=f-taxi-v3 ++seed=7301
+
+
