9e3dc0e42908a049f5dbe58a3cbd653a9df479d2
Guillermo <guillermo.infante@upf.edu>
2024-02-07 18:01:55 +0100

    modified conf files

--- run_flat_online.sh
+++ run_flat_online.sh
@@ -1,11 +1,11 @@
 # NRoom-v0
-python online_flat.py +experiment=flat-log-nroom-v0 ++seed=1001
-python online_flat.py +experiment=flat-log-nroom-v0 ++seed=5739
-python online_flat.py +experiment=flat-log-nroom-v0 ++seed=8101
+# python online_flat.py +experiment=flat-log-nroom-v0 ++seed=1001
+# python online_flat.py +experiment=flat-log-nroom-v0 ++seed=5739
+# python online_flat.py +experiment=flat-log-nroom-v0 ++seed=8101
 
-python online_flat.py +experiment=flat-exp-nroom-v0 ++seed=1001
-python online_flat.py +experiment=flat-exp-nroom-v0 ++seed=5739
-python online_flat.py +experiment=flat-exp-nroom-v0 ++seed=8101
+# python online_flat.py +experiment=flat-exp-nroom-v0 ++seed=1001
+# python online_flat.py +experiment=flat-exp-nroom-v0 ++seed=5739
+# python online_flat.py +experiment=flat-exp-nroom-v0 ++seed=8101
 
 # NRoom-v1
 python online_flat.py +experiment=flat-log-nroom-v1 ++seed=2222
--- run_hierarchical_online.sh
+++ run_hierarchical_online.sh
@@ -34,6 +34,19 @@
 # python online_hierarchical.py +experiment=h-exp-nroom-v2 ++seed=7189
 # python online_hierarchical.py +experiment=h-exp-nroom-v2 ++seed=7301
 
+python online_hierarchical.py +experiment=h-log-nroom-v3 ++seed=1001
+python online_hierarchical.py +experiment=h-log-nroom-v3 ++seed=2222
+python online_hierarchical.py +experiment=h-log-nroom-v3 ++seed=9181
+python online_hierarchical.py +experiment=h-log-nroom-v3 ++seed=7189
+python online_hierarchical.py +experiment=h-log-nroom-v3 ++seed=7301
+
+python online_hierarchical.py +experiment=h-exp-nroom-v3 ++seed=1001
+python online_hierarchical.py +experiment=h-exp-nroom-v3 ++seed=2222
+python online_hierarchical.py +experiment=h-exp-nroom-v3 ++seed=9181
+python online_hierarchical.py +experiment=h-exp-nroom-v3 ++seed=7189
+python online_hierarchical.py +experiment=h-exp-nroom-v3 ++seed=7301
+
+
 # python online_hierarchical.py +experiment=h-exp-taxi-v0 ++seed=1001
 # python online_hierarchical.py +experiment=h-exp-taxi-v0 ++seed=2222
 # python online_hierarchical.py +experiment=h-exp-taxi-v0 ++seed=9181
