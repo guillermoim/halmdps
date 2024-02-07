4a260790ec2ed64af58ea7942a2005ad0032e176
Guillermo <guillermo.infante@upf.edu>
2024-02-07 16:53:28 +0100

    added more confs

--- run_flat_online.sh
+++ run_flat_online.sh
@@ -1,41 +1,73 @@
-# python online_flat.py +experiment=flat-log-nroom-v0 ++seed=1001
-# python online_flat.py +experiment=flat-log-nroom-v0 ++seed=5739
-# python online_flat.py +experiment=flat-log-nroom-v0 ++seed=8101
+# NRoom-v0
+python online_flat.py +experiment=flat-log-nroom-v0 ++seed=1001
+python online_flat.py +experiment=flat-log-nroom-v0 ++seed=5739
+python online_flat.py +experiment=flat-log-nroom-v0 ++seed=8101
 
 python online_flat.py +experiment=flat-exp-nroom-v0 ++seed=1001
 python online_flat.py +experiment=flat-exp-nroom-v0 ++seed=5739
 python online_flat.py +experiment=flat-exp-nroom-v0 ++seed=8101
 
-# python online_flat.py +experiment=flat-log-nroom-v0 ++seed=2222
-# python online_flat.py +experiment=flat-log-nroom-v0 ++seed=5739
-# python online_flat.py +experiment=flat-log-nroom-v0 ++seed=8101
+# NRoom-v1
+python online_flat.py +experiment=flat-log-nroom-v1 ++seed=2222
+python online_flat.py +experiment=flat-log-nroom-v1 ++seed=5739
+python online_flat.py +experiment=flat-log-nroom-v1 ++seed=8101
 
-# python online_flat.py +experiment=flat-exp-nroom-v1 ++seed=1001
-# python online_flat.py +experiment=flat-exp-nroom-v1 ++seed=2222
-# python online_flat.py +experiment=flat-exp-nroom-v1 ++seed=5739
-# python online_flat.py +experiment=flat-exp-nroom-v1 ++seed=8101
+python online_flat.py +experiment=flat-exp-nroom-v1 ++seed=1001
+python online_flat.py +experiment=flat-exp-nroom-v1 ++seed=2222
+python online_flat.py +experiment=flat-exp-nroom-v1 ++seed=5739
+python online_flat.py +experiment=flat-exp-nroom-v1 ++seed=8101
 
-# python online_flat.py +experiment=flat-exp-taxi-v0 ++seed=1001
-# python online_flat.py +experiment=flat-exp-taxi-v0 ++seed=2222
-# python online_flat.py +experiment=flat-exp-taxi-v0 ++seed=5739
-# python online_flat.py +experiment=flat-exp-taxi-v0 ++seed=8101
+# NRoom-v2
+python online_flat.py +experiment=flat-log-nroom-v2 ++seed=2222
+python online_flat.py +experiment=flat-log-nroom-v2 ++seed=5739
+python online_flat.py +experiment=flat-log-nroom-v2 ++seed=8101
 
-# python online_flat.py +experiment=flat-exp-taxi-v1 ++seed=1001
-# python online_flat.py +experiment=flat-exp-taxi-v1 ++seed=2222
-# python online_flat.py +experiment=flat-exp-taxi-v1 ++seed=5739
-# python online_flat.py +experiment=flat-exp-taxi-v1 ++seed=8101
+python online_flat.py +experiment=flat-exp-nroom-v2 ++seed=1001
+python online_flat.py +experiment=flat-exp-nroom-v2 ++seed=2222
+python online_flat.py +experiment=flat-exp-nroom-v2 ++seed=5739
+python online_flat.py +experiment=flat-exp-nroom-v2 ++seed=8101
 
-# python online_flat.py +experiment=flat-exp-nroom-v2 ++seed=1001
-# python online_flat.py +experiment=flat-exp-nroom-v2 ++seed=2222
-# python online_flat.py +experiment=flat-exp-nroom-v2 ++seed=5739
-# python online_flat.py +experiment=flat-exp-nroom-v2 ++seed=8101
+# Taxi v0
+python online_flat.py +experiment=flat-log-taxi-v0 ++seed=1001
+python online_flat.py +experiment=flat-log-taxi-v0 ++seed=2222
+python online_flat.py +experiment=flat-log-taxi-v0 ++seed=5739
+python online_flat.py +experiment=flat-log-taxi-v0 ++seed=8101
 
-# python online_flat.py +experiment=flat-exp-nroom-v3 ++seed=1001
-# python online_flat.py +experiment=flat-exp-nroom-v3 ++seed=2222
-# python online_flat.py +experiment=flat-exp-nroom-v3 ++seed=5739
-# python online_flat.py +experiment=flat-exp-nroom-v3 ++seed=8101
+python online_flat.py +experiment=flat-exp-taxi-v0 ++seed=1001
+python online_flat.py +experiment=flat-exp-taxi-v0 ++seed=2222
+python online_flat.py +experiment=flat-exp-taxi-v0 ++seed=5739
+python online_flat.py +experiment=flat-exp-taxi-v0 ++seed=8101
 
-# python online_flat.py +experiment=flat-exp-taxi-v2 ++seed=1001
-# python online_flat.py +experiment=flat-exp-taxi-v2 ++seed=2222
-# python online_flat.py +experiment=flat-exp-taxi-v2 ++seed=5739
-# python online_flat.py +experiment=flat-exp-taxi-v2 ++seed=8101
+# Taxi v1
+python online_flat.py +experiment=flat-log-taxi-v1 ++seed=1001
+python online_flat.py +experiment=flat-log-taxi-v1 ++seed=2222
+python online_flat.py +experiment=flat-log-taxi-v1 ++seed=5739
+python online_flat.py +experiment=flat-log-taxi-v1 ++seed=8101
+
+python online_flat.py +experiment=flat-exp-taxi-v1 ++seed=1001
+python online_flat.py +experiment=flat-exp-taxi-v1 ++seed=2222
+python online_flat.py +experiment=flat-exp-taxi-v1 ++seed=5739
+python online_flat.py +experiment=flat-exp-taxi-v1 ++seed=8101
+
+# Taxi v2
+python online_flat.py +experiment=flat-exp-taxi-v2 ++seed=1001
+python online_flat.py +experiment=flat-exp-taxi-v2 ++seed=2222
+python online_flat.py +experiment=flat-exp-taxi-v2 ++seed=5739
+python online_flat.py +experiment=flat-exp-taxi-v2 ++seed=8101
+
+
+python online_flat.py +experiment=flat-log-taxi-v2 ++seed=1001
+python online_flat.py +experiment=flat-log-taxi-v2 ++seed=2222
+python online_flat.py +experiment=flat-log-taxi-v2 ++seed=5739
+python online_flat.py +experiment=flat-log-taxi-v2 ++seed=8101
+
+
+# NRoom-v3
+python online_flat.py +experiment=flat-log-nroom-v3 ++seed=2222
+python online_flat.py +experiment=flat-log-nroom-v3 ++seed=5739
+python online_flat.py +experiment=flat-log-nroom-v3 ++seed=8101
+
+python online_flat.py +experiment=flat-exp-nroom-v3 ++seed=1001
+python online_flat.py +experiment=flat-exp-nroom-v3 ++seed=2222
+python online_flat.py +experiment=flat-exp-nroom-v3 ++seed=5739
+python online_flat.py +experiment=flat-exp-nroom-v3 ++seed=8101
--- run_hierarchical_online.sh
+++ run_hierarchical_online.sh
@@ -47,17 +47,29 @@
 # python online_hierarchical.py +experiment=h-log-taxi-v0 ++seed=7301
 # # 
 # 
-python online_hierarchical.py +experiment=h-log-taxi-v1 ++seed=1001
-python online_hierarchical.py +experiment=h-log-taxi-v1 ++seed=2222
-python online_hierarchical.py +experiment=h-log-taxi-v1 ++seed=9181
-python online_hierarchical.py +experiment=h-log-taxi-v1 ++seed=7189
-python online_hierarchical.py +experiment=h-log-taxi-v1 ++seed=7301
+# python online_hierarchical.py +experiment=h-log-taxi-v1 ++seed=1001
+# python online_hierarchical.py +experiment=h-log-taxi-v1 ++seed=2222
+# python online_hierarchical.py +experiment=h-log-taxi-v1 ++seed=9181
+# python online_hierarchical.py +experiment=h-log-taxi-v1 ++seed=7189
+# python online_hierarchical.py +experiment=h-log-taxi-v1 ++seed=7301
 
-python online_hierarchical.py +experiment=h-exp-taxi-v1 ++seed=1001
-python online_hierarchical.py +experiment=h-exp-taxi-v1 ++seed=2222
-python online_hierarchical.py +experiment=h-exp-taxi-v1 ++seed=9181
-python online_hierarchical.py +experiment=h-exp-taxi-v1 ++seed=7189
-python online_hierarchical.py +experiment=h-exp-taxi-v1 ++seed=7301
+# python online_hierarchical.py +experiment=h-exp-taxi-v1 ++seed=1001
+# python online_hierarchical.py +experiment=h-exp-taxi-v1 ++seed=2222
+# python online_hierarchical.py +experiment=h-exp-taxi-v1 ++seed=9181
+# python online_hierarchical.py +experiment=h-exp-taxi-v1 ++seed=7189
+# python online_hierarchical.py +experiment=h-exp-taxi-v1 ++seed=7301
+
+python online_hierarchical.py +experiment=h-log-taxi-v2 ++seed=1001
+python online_hierarchical.py +experiment=h-log-taxi-v2 ++seed=2222
+python online_hierarchical.py +experiment=h-log-taxi-v2 ++seed=9181
+python online_hierarchical.py +experiment=h-log-taxi-v2 ++seed=7189
+python online_hierarchical.py +experiment=h-log-taxi-v2 ++seed=7301
+
+python online_hierarchical.py +experiment=h-exp-taxi-v2 ++seed=1001
+python online_hierarchical.py +experiment=h-exp-taxi-v2 ++seed=2222
+python online_hierarchical.py +experiment=h-exp-taxi-v2 ++seed=9181
+python online_hierarchical.py +experiment=h-exp-taxi-v2 ++seed=7189
+python online_hierarchical.py +experiment=h-exp-taxi-v2 ++seed=7301
 
 
 
