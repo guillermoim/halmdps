0b64a53b55211b517ac51394a8ceecbdd1f97b76
Guillermo <guillermo.infante@upf.edu>
2024-02-07 15:49:36 +0100

    Added gt results

--- run_hierarchical_online.sh
+++ run_hierarchical_online.sh
@@ -28,25 +28,24 @@
 # python online_hierarchical.py +experiment=h-log-nroom-v2 ++seed=7189
 # python online_hierarchical.py +experiment=h-log-nroom-v2 ++seed=7301
 
-python online_hierarchical.py +experiment=h-exp-nroom-v2 ++seed=1001
-python online_hierarchical.py +experiment=h-exp-nroom-v2 ++seed=2222
-python online_hierarchical.py +experiment=h-exp-nroom-v2 ++seed=9181
-python online_hierarchical.py +experiment=h-exp-nroom-v2 ++seed=7189
-python online_hierarchical.py +experiment=h-exp-nroom-v2 ++seed=7301
+# python online_hierarchical.py +experiment=h-exp-nroom-v2 ++seed=1001
+# python online_hierarchical.py +experiment=h-exp-nroom-v2 ++seed=2222
+# python online_hierarchical.py +experiment=h-exp-nroom-v2 ++seed=9181
+# python online_hierarchical.py +experiment=h-exp-nroom-v2 ++seed=7189
+# python online_hierarchical.py +experiment=h-exp-nroom-v2 ++seed=7301
 
+# python online_hierarchical.py +experiment=h-exp-taxi-v0 ++seed=1001
+# python online_hierarchical.py +experiment=h-exp-taxi-v0 ++seed=2222
+# python online_hierarchical.py +experiment=h-exp-taxi-v0 ++seed=9181
+# python online_hierarchical.py +experiment=h-exp-taxi-v0 ++seed=7189
+# python online_hierarchical.py +experiment=h-exp-taxi-v0 ++seed=7301
 
-python online_hierarchical.py +experiment=h-exp-taxi-v0 ++seed=1001
-python online_hierarchical.py +experiment=h-exp-taxi-v0 ++seed=2222
-python online_hierarchical.py +experiment=h-exp-taxi-v0 ++seed=9181
-python online_hierarchical.py +experiment=h-exp-taxi-v0 ++seed=7189
-python online_hierarchical.py +experiment=h-exp-taxi-v0 ++seed=7301
-
-python online_hierarchical.py +experiment=h-log-taxi-v0 ++seed=1001
-python online_hierarchical.py +experiment=h-log-taxi-v0 ++seed=2222
-python online_hierarchical.py +experiment=h-log-taxi-v0 ++seed=9181
-python online_hierarchical.py +experiment=h-log-taxi-v0 ++seed=7189
-python online_hierarchical.py +experiment=h-log-taxi-v0 ++seed=7301
-# 
+# python online_hierarchical.py +experiment=h-log-taxi-v0 ++seed=1001
+# python online_hierarchical.py +experiment=h-log-taxi-v0 ++seed=2222
+# python online_hierarchical.py +experiment=h-log-taxi-v0 ++seed=9181
+# python online_hierarchical.py +experiment=h-log-taxi-v0 ++seed=7189
+# python online_hierarchical.py +experiment=h-log-taxi-v0 ++seed=7301
+# # 
 # 
 python online_hierarchical.py +experiment=h-log-taxi-v1 ++seed=1001
 python online_hierarchical.py +experiment=h-log-taxi-v1 ++seed=2222
