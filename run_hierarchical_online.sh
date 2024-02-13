8d65e723bc2891f23426ec860dc48a152764fc6e
Guillermo <guillermo.infante@upf.edu>
2024-02-13 12:54:02 +0100

    changed conf files

--- run_hierarchical_online.sh
+++ run_hierarchical_online.sh
@@ -72,17 +72,17 @@ python online_hierarchical.py +experiment=h-exp-nroom-v3 ++seed=7301
 # python online_hierarchical.py +experiment=h-exp-taxi-v1 ++seed=7189
 # python online_hierarchical.py +experiment=h-exp-taxi-v1 ++seed=7301
 
-python online_hierarchical.py +experiment=h-log-taxi-v2 ++seed=1001
-python online_hierarchical.py +experiment=h-log-taxi-v2 ++seed=2222
-python online_hierarchical.py +experiment=h-log-taxi-v2 ++seed=9181
-python online_hierarchical.py +experiment=h-log-taxi-v2 ++seed=7189
-python online_hierarchical.py +experiment=h-log-taxi-v2 ++seed=7301
+# python online_hierarchical.py +experiment=h-log-taxi-v2 ++seed=1001
+# python online_hierarchical.py +experiment=h-log-taxi-v2 ++seed=2222
+# python online_hierarchical.py +experiment=h-log-taxi-v2 ++seed=9181
+# python online_hierarchical.py +experiment=h-log-taxi-v2 ++seed=7189
+# python online_hierarchical.py +experiment=h-log-taxi-v2 ++seed=7301
 
-python online_hierarchical.py +experiment=h-exp-taxi-v2 ++seed=1001
-python online_hierarchical.py +experiment=h-exp-taxi-v2 ++seed=2222
-python online_hierarchical.py +experiment=h-exp-taxi-v2 ++seed=9181
-python online_hierarchical.py +experiment=h-exp-taxi-v2 ++seed=7189
-python online_hierarchical.py +experiment=h-exp-taxi-v2 ++seed=7301
+# python online_hierarchical.py +experiment=h-exp-taxi-v2 ++seed=1001
+# python online_hierarchical.py +experiment=h-exp-taxi-v2 ++seed=2222
+# python online_hierarchical.py +experiment=h-exp-taxi-v2 ++seed=9181
+# python online_hierarchical.py +experiment=h-exp-taxi-v2 ++seed=7189
+# python online_hierarchical.py +experiment=h-exp-taxi-v2 ++seed=7301
 
 
 
