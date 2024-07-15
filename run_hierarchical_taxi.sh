e3af1409cc9f7bd52664f2ad68adc92fd782fffa
Guillermo <guillermo.infante@upf.edu>
2024-07-15 11:59:43 +0200

    modified script taxi

--- run_hierarchical_taxi.sh
+++ run_hierarchical_taxi.sh
@@ -11,19 +11,50 @@
 # python online_hierarchical.py +experiment=h-log-short ++env_name=Taxi-v1 ++n_samples=1e6 ++seed=7189
 # python online_hierarchical.py +experiment=h-log-short ++env_name=Taxi-v1 ++n_samples=1e6 ++seed=7301
 
+python online_hierarchical.py +experiment=h-exp-short ++env_name=Taxi-v1 ++n_samples=1e6 ++seed=1001 k=1e-1 
+python online_hierarchical.py +experiment=h-exp-short ++env_name=Taxi-v1 ++n_samples=1e6 ++seed=2222 k=1e-1
+python online_hierarchical.py +experiment=h-exp-short ++env_name=Taxi-v1 ++n_samples=1e6 ++seed=9181 k=1e-1
+python online_hierarchical.py +experiment=h-exp-short ++env_name=Taxi-v1 ++n_samples=1e6 ++seed=7189 k=1e-1
+python online_hierarchical.py +experiment=h-exp-short ++env_name=Taxi-v1 ++n_samples=1e6 ++seed=7301 k=1e-1
+
+python online_hierarchical.py +experiment=h-exp-short ++env_name=Taxi-v1 ++n_samples=1e6 ++seed=1001 k=1e-2 
+python online_hierarchical.py +experiment=h-exp-short ++env_name=Taxi-v1 ++n_samples=1e6 ++seed=2222 k=1e-2
+python online_hierarchical.py +experiment=h-exp-short ++env_name=Taxi-v1 ++n_samples=1e6 ++seed=9181 k=1e-2
+python online_hierarchical.py +experiment=h-exp-short ++env_name=Taxi-v1 ++n_samples=1e6 ++seed=7189 k=1e-2
+python online_hierarchical.py +experiment=h-exp-short ++env_name=Taxi-v1 ++n_samples=1e6 ++seed=7301 k=1e-2
+
 
 # Taxi-v2
-# python online_hierarchical.py +experiment=h-exp-large ++env_name=Taxi-v2 ++n_samples=5e6 ++seed=1001
-# python online_hierarchical.py +experiment=h-exp-large ++env_name=Taxi-v2 ++n_samples=5e6 ++seed=2222
-# python online_hierarchical.py +experiment=h-exp-large ++env_name=Taxi-v2 ++n_samples=5e6 ++seed=9181
-# python online_hierarchical.py +experiment=h-exp-large ++env_name=Taxi-v2 ++n_samples=5e6 ++seed=7189
-# python online_hierarchical.py +experiment=h-exp-large ++env_name=Taxi-v2 ++n_samples=5e6 ++seed=7301
+python online_hierarchical.py +experiment=h-exp-large ++env_name=Taxi-v2 ++n_samples=5e6 ++seed=1001 k=1e-1
+python online_hierarchical.py +experiment=h-exp-large ++env_name=Taxi-v2 ++n_samples=5e6 ++seed=2222 k=1e-1
+python online_hierarchical.py +experiment=h-exp-large ++env_name=Taxi-v2 ++n_samples=5e6 ++seed=9181 k=1e-1
+python online_hierarchical.py +experiment=h-exp-large ++env_name=Taxi-v2 ++n_samples=5e6 ++seed=7189 k=1e-1
+python online_hierarchical.py +experiment=h-exp-large ++env_name=Taxi-v2 ++n_samples=5e6 ++seed=7301 k=1e-1
 
-# python online_hierarchical.py +experiment=h-log-large ++env_name=Taxi-v2 ++n_samples=5e6 ++seed=1001
-# python online_hierarchical.py +experiment=h-log-large ++env_name=Taxi-v2 ++n_samples=5e6 ++seed=2222
-# python online_hierarchical.py +experiment=h-log-large ++env_name=Taxi-v2 ++n_samples=5e6 ++seed=9181
-# python online_hierarchical.py +experiment=h-log-large ++env_name=Taxi-v2 ++n_samples=5e6 ++seed=7189
-# python online_hierarchical.py +experiment=h-log-large ++env_name=Taxi-v2 ++n_samples=5e6 ++seed=7301
+python online_hierarchical.py +experiment=h-exp-large ++env_name=Taxi-v2 ++n_samples=5e6 ++seed=1001 k=1e-2
+python online_hierarchical.py +experiment=h-exp-large ++env_name=Taxi-v2 ++n_samples=5e6 ++seed=2222 k=1e-2
+python online_hierarchical.py +experiment=h-exp-large ++env_name=Taxi-v2 ++n_samples=5e6 ++seed=9181 k=1e-2
+python online_hierarchical.py +experiment=h-exp-large ++env_name=Taxi-v2 ++n_samples=5e6 ++seed=7189 k=1e-2
+python online_hierarchical.py +experiment=h-exp-large ++env_name=Taxi-v2 ++n_samples=5e6 ++seed=7301 k=1e-2
+
+python online_hierarchical.py +experiment=h-exp-large ++env_name=Taxi-v2 ++n_samples=5e6 ++seed=1001 k=1e-4
+python online_hierarchical.py +experiment=h-exp-large ++env_name=Taxi-v2 ++n_samples=5e6 ++seed=2222 k=1e-4
+python online_hierarchical.py +experiment=h-exp-large ++env_name=Taxi-v2 ++n_samples=5e6 ++seed=9181 k=1e-4
+python online_hierarchical.py +experiment=h-exp-large ++env_name=Taxi-v2 ++n_samples=5e6 ++seed=7189 k=1e-4
+python online_hierarchical.py +experiment=h-exp-large ++env_name=Taxi-v2 ++n_samples=5e6 ++seed=7301 k=1e-4
+
+python online_hierarchical.py +experiment=h-exp-large ++env_name=Taxi-v2 ++n_samples=5e6 ++seed=1001 k=1e-6
+python online_hierarchical.py +experiment=h-exp-large ++env_name=Taxi-v2 ++n_samples=5e6 ++seed=2222 k=1e-6
+python online_hierarchical.py +experiment=h-exp-large ++env_name=Taxi-v2 ++n_samples=5e6 ++seed=9181 k=1e-6
+python online_hierarchical.py +experiment=h-exp-large ++env_name=Taxi-v2 ++n_samples=5e6 ++seed=7189 k=1e-6
+python online_hierarchical.py +experiment=h-exp-large ++env_name=Taxi-v2 ++n_samples=5e6 ++seed=7301 k=1e-6
+
+
+python online_hierarchical.py +experiment=h-exp-large ++env_name=Taxi-v2 ++n_samples=5e6 ++seed=1001 k=1e-8
+python online_hierarchical.py +experiment=h-exp-large ++env_name=Taxi-v2 ++n_samples=5e6 ++seed=2222 k=1e-8
+python online_hierarchical.py +experiment=h-exp-large ++env_name=Taxi-v2 ++n_samples=5e6 ++seed=9181 k=1e-8
+python online_hierarchical.py +experiment=h-exp-large ++env_name=Taxi-v2 ++n_samples=5e6 ++seed=7189 k=1e-8
+# python online_hierarchical.py +experiment=h-exp-large ++env_name=Taxi-v2 ++n_samples=5e6 ++seed=7301 k=1e-8
 
 
 # python online_hierarchical.py +experiment=h-log-large ++env_name=Taxi-v3 ++n_samples=1e6 ++seed=1001
@@ -32,11 +63,30 @@
 # python online_hierarchical.py +experiment=h-log-large ++env_name=Taxi-v3 ++n_samples=1e6 ++seed=7189
 # python online_hierarchical.py +experiment=h-log-large ++env_name=Taxi-v3 ++n_samples=1e6 ++seed=7301
 
+python online_hierarchical.py +experiment=h-exp-large ++env_name=Taxi-v3 ++n_samples=5e6 ++seed=1001  ++k=1e-1
+python online_hierarchical.py +experiment=h-exp-large ++env_name=Taxi-v3 ++n_samples=5e6 ++seed=2222  ++k=1e-1
+python online_hierarchical.py +experiment=h-exp-large ++env_name=Taxi-v3 ++n_samples=5e6 ++seed=9181  ++k=1e-1
+python online_hierarchical.py +experiment=h-exp-large ++env_name=Taxi-v3 ++n_samples=5e6 ++seed=7189  ++k=1e-1
+python online_hierarchical.py +experiment=h-exp-large ++env_name=Taxi-v3 ++n_samples=5e6 ++seed=7301  ++k=1e-1
+
+python online_hierarchical.py +experiment=h-exp-large ++env_name=Taxi-v3 ++n_samples=5e6 ++seed=1001  ++k=1e-2
+python online_hierarchical.py +experiment=h-exp-large ++env_name=Taxi-v3 ++n_samples=5e6 ++seed=2222  ++k=1e-2
+python online_hierarchical.py +experiment=h-exp-large ++env_name=Taxi-v3 ++n_samples=5e6 ++seed=9181  ++k=1e-2
+python online_hierarchical.py +experiment=h-exp-large ++env_name=Taxi-v3 ++n_samples=5e6 ++seed=7189  ++k=1e-2
+python online_hierarchical.py +experiment=h-exp-large ++env_name=Taxi-v3 ++n_samples=5e6 ++seed=7301  ++k=1e-2
+
+python online_hierarchical.py +experiment=h-exp-large ++env_name=Taxi-v3 ++n_samples=5e6 ++seed=1001  ++k=1e-4
+python online_hierarchical.py +experiment=h-exp-large ++env_name=Taxi-v3 ++n_samples=5e6 ++seed=2222  ++k=1e-4
+python online_hierarchical.py +experiment=h-exp-large ++env_name=Taxi-v3 ++n_samples=5e6 ++seed=9181  ++k=1e-4
+python online_hierarchical.py +experiment=h-exp-large ++env_name=Taxi-v3 ++n_samples=5e6 ++seed=7189  ++k=1e-4
+python online_hierarchical.py +experiment=h-exp-large ++env_name=Taxi-v3 ++n_samples=5e6 ++seed=7301  ++k=1e-4
+
 python online_hierarchical.py +experiment=h-exp-large ++env_name=Taxi-v3 ++n_samples=5e6 ++seed=1001  ++k=1e-6
 python online_hierarchical.py +experiment=h-exp-large ++env_name=Taxi-v3 ++n_samples=5e6 ++seed=2222  ++k=1e-6
 python online_hierarchical.py +experiment=h-exp-large ++env_name=Taxi-v3 ++n_samples=5e6 ++seed=9181  ++k=1e-6
 python online_hierarchical.py +experiment=h-exp-large ++env_name=Taxi-v3 ++n_samples=5e6 ++seed=7189  ++k=1e-6
 python online_hierarchical.py +experiment=h-exp-large ++env_name=Taxi-v3 ++n_samples=5e6 ++seed=7301  ++k=1e-6
+
 python online_hierarchical.py +experiment=h-exp-large ++env_name=Taxi-v3 ++n_samples=5e6 ++seed=1001  ++k=1e-8
 python online_hierarchical.py +experiment=h-exp-large ++env_name=Taxi-v3 ++n_samples=5e6 ++seed=2222  ++k=1e-8 
 python online_hierarchical.py +experiment=h-exp-large ++env_name=Taxi-v3 ++n_samples=5e6 ++seed=9181  ++k=1e-8
