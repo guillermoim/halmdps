3e2679f658443edf8a3b927ab4b4791690dc84b9
Guillermo <guillermo.infante@upf.edu>
2024-07-16 17:31:20 +0200

    added nroom script

--- run_hierarchical_nroom.sh
+++ run_hierarchical_nroom.sh
@@ -1,9 +1,16 @@
 # NRoom-v1
-python online_hierarchical.py +experiment=h-exp-short ++env_name=NRoom-v1 ++n_samples=5e5 ++seed=1001 k=1e-1
-python online_hierarchical.py +experiment=h-exp-short ++env_name=NRoom-v1 ++n_samples=5e5 ++seed=2222 k=1e-1 
-python online_hierarchical.py +experiment=h-exp-short ++env_name=NRoom-v1 ++n_samples=5e5 ++seed=9181 k=1e-1
-python online_hierarchical.py +experiment=h-exp-short ++env_name=NRoom-v1 ++n_samples=5e5 ++seed=7189 k=1e-1
-python online_hierarchical.py +experiment=h-exp-short ++env_name=NRoom-v1 ++n_samples=5e5 ++seed=7301 k=1e-1
+python online_hierarchical.py +experiment=h-exp-short ++env_name=NRoom-v1 ++n_samples=1e6 ++seed=1001
+python online_hierarchical.py +experiment=h-exp-short ++env_name=NRoom-v1 ++n_samples=1e6 ++seed=2222 
+python online_hierarchical.py +experiment=h-exp-short ++env_name=NRoom-v1 ++n_samples=1e6 ++seed=9181
+python online_hierarchical.py +experiment=h-exp-short ++env_name=NRoom-v1 ++n_samples=1e6 ++seed=7189
+python online_hierarchical.py +experiment=h-exp-short ++env_name=NRoom-v1 ++n_samples=1e6 ++seed=7301
+
+python online_hierarchical.py +experiment=h-log-short ++env_name=NRoom-v1 ++n_samples=1e6 ++seed=1001 k=1e-1
+python online_hierarchical.py +experiment=h-log-short ++env_name=NRoom-v1 ++n_samples=1e6 ++seed=2222 k=1e-1 
+python online_hierarchical.py +experiment=h-log-short ++env_name=NRoom-v1 ++n_samples=1e6 ++seed=9181 k=1e-1
+python online_hierarchical.py +experiment=h-log-short ++env_name=NRoom-v1 ++n_samples=1e6 ++seed=7189 k=1e-1
+python online_hierarchical.py +experiment=h-log-short ++env_name=NRoom-v1 ++n_samples=1e6 ++seed=7301 k=1e-1
+
 
 # python online_hierarchical.py +experiment=h-log-short ++env_name=NRoom-v1 ++n_samples=1e6 ++seed=1001
 # python online_hierarchical.py +experiment=h-log-short ++env_name=NRoom-v1 ++n_samples=1e6 ++seed=2222
@@ -13,31 +20,18 @@ python online_hierarchical.py +experiment=h-exp-short ++env_name=NRoom-v1 ++n_sa
 
 
 # # NRoom-v2
-python online_hierarchical.py +experiment=h-exp-short ++env_name=NRoom-v2 ++n_samples=5e5 ++seed=1001 k=1e-1
-python online_hierarchical.py +experiment=h-exp-short ++env_name=NRoom-v2 ++n_samples=5e5 ++seed=2222 k=1e-1 
-python online_hierarchical.py +experiment=h-exp-short ++env_name=NRoom-v2 ++n_samples=5e5 ++seed=9181 k=1e-1
-python online_hierarchical.py +experiment=h-exp-short ++env_name=NRoom-v2 ++n_samples=5e5 ++seed=7189 k=1e-1
-python online_hierarchical.py +experiment=h-exp-short ++env_name=NRoom-v2 ++n_samples=5e5 ++seed=7301 k=1e-1
+python online_hierarchical.py +experiment=h-exp-large ++experiment.lrs.schedule3=300 ++env_name=NRoom-v2 ++n_samples=1e6 ++seed=1001 
+python online_hierarchical.py +experiment=h-exp-short ++env_name=NRoom-v2 ++n_samples=1e6 ++seed=1001 
+python online_hierarchical.py +experiment=h-exp-short ++env_name=NRoom-v2 ++n_samples=1e6 ++seed=2222  
+python online_hierarchical.py +experiment=h-exp-short ++env_name=NRoom-v2 ++n_samples=1e6 ++seed=9181 
+python online_hierarchical.py +experiment=h-exp-short ++env_name=NRoom-v2 ++n_samples=1e6 ++seed=7189
+python online_hierarchical.py +experiment=h-exp-short ++env_name=NRoom-v2 ++n_samples=1e6 ++seed=7301
 
-python online_hierarchical.py +experiment=h-exp-short ++env_name=NRoom-v2 ++n_samples=5e5 ++seed=1001 k=1e-2
-python online_hierarchical.py +experiment=h-exp-short ++env_name=NRoom-v2 ++n_samples=5e5 ++seed=2222 k=1e-2 
-python online_hierarchical.py +experiment=h-exp-short ++env_name=NRoom-v2 ++n_samples=5e5 ++seed=9181 k=1e-2
-python online_hierarchical.py +experiment=h-exp-short ++env_name=NRoom-v2 ++n_samples=5e5 ++seed=7189 k=1e-2
-python online_hierarchical.py +experiment=h-exp-short ++env_name=NRoom-v2 ++n_samples=5e5 ++seed=7301 k=1e-2
-
-
-python online_hierarchical.py +experiment=h-exp-short ++env_name=NRoom-v2 ++n_samples=5e5 ++seed=1001 k=1e-3
-python online_hierarchical.py +experiment=h-exp-short ++env_name=NRoom-v2 ++n_samples=5e5 ++seed=2222 k=1e-3 
-python online_hierarchical.py +experiment=h-exp-short ++env_name=NRoom-v2 ++n_samples=5e5 ++seed=9181 k=1e-3
-python online_hierarchical.py +experiment=h-exp-short ++env_name=NRoom-v2 ++n_samples=5e5 ++seed=7189 k=1e-3
-python online_hierarchical.py +experiment=h-exp-short ++env_name=NRoom-v2 ++n_samples=5e5 ++seed=7301 k=1e-3
-
-
-# python online_hierarchical.py +experiment=h-log-short ++env_name=NRoom-v2 ++n_samples=1e6 ++seed=1001
-# python online_hierarchical.py +experiment=h-log-short ++env_name=NRoom-v2 ++n_samples=1e6 ++seed=2222
-# python online_hierarchical.py +experiment=h-log-short ++env_name=NRoom-v2 ++n_samples=1e6 ++seed=9181
-# python online_hierarchical.py +experiment=h-log-short ++env_name=NRoom-v2 ++n_samples=1e6 ++seed=7189
-# python online_hierarchical.py +experiment=h-log-short ++env_name=NRoom-v2 ++n_samples=1e6 ++seed=7301
+python online_hierarchical.py +experiment=h-log-short ++env_name=NRoom-v2 ++n_samples=1e6 ++seed=1001 k=1e-1
+python online_hierarchical.py +experiment=h-log-short ++env_name=NRoom-v2 ++n_samples=1e6 ++seed=2222 k=1e-1
+python online_hierarchical.py +experiment=h-log-short ++env_name=NRoom-v2 ++n_samples=1e6 ++seed=9181 k=1e-1
+python online_hierarchical.py +experiment=h-log-short ++env_name=NRoom-v2 ++n_samples=1e6 ++seed=7189 k=1e-1
+python online_hierarchical.py +experiment=h-log-short ++env_name=NRoom-v2 ++n_samples=1e6 ++seed=7301 k=1e-1
 
 # # NRoom-v3
 python online_hierarchical.py +experiment=h-exp-large ++env_name=NRoom-v3 ++n_samples=1e6 ++seed=1001 k=1e-1
@@ -51,27 +45,3 @@ python online_hierarchical.py +experiment=h-log-large ++env_name=NRoom-v3 ++n_sa
 python online_hierarchical.py +experiment=h-log-large ++env_name=NRoom-v3 ++n_samples=1e6 ++seed=9181 k=1e-1
 python online_hierarchical.py +experiment=h-log-large ++env_name=NRoom-v3 ++n_samples=1e6 ++seed=7189 k=1e-1
 python online_hierarchical.py +experiment=h-log-large ++env_name=NRoom-v3 ++n_samples=1e6 ++seed=7301 k=1e-1
-
-python online_hierarchical.py +experiment=h-exp-large ++env_name=NRoom-v3 ++n_samples=1e6 ++seed=1001 k=1e-2
-python online_hierarchical.py +experiment=h-exp-large ++env_name=NRoom-v3 ++n_samples=1e6 ++seed=2222 k=1e-2
-python online_hierarchical.py +experiment=h-exp-large ++env_name=NRoom-v3 ++n_samples=1e6 ++seed=9181 k=1e-2
-python online_hierarchical.py +experiment=h-exp-large ++env_name=NRoom-v3 ++n_samples=1e6 ++seed=7189 k=1e-2
-python online_hierarchical.py +experiment=h-exp-large ++env_name=NRoom-v3 ++n_samples=1e6 ++seed=7301 k=1e-2
-
-python online_hierarchical.py +experiment=h-log-large ++env_name=NRoom-v3 ++n_samples=1e6 ++seed=1001 k=1e-2
-python online_hierarchical.py +experiment=h-log-large ++env_name=NRoom-v3 ++n_samples=1e6 ++seed=2222 k=1e-2
-python online_hierarchical.py +experiment=h-log-large ++env_name=NRoom-v3 ++n_samples=1e6 ++seed=9181 k=1e-2
-python online_hierarchical.py +experiment=h-log-large ++env_name=NRoom-v3 ++n_samples=1e6 ++seed=7189 k=1e-2
-python online_hierarchical.py +experiment=h-log-large ++env_name=NRoom-v3 ++n_samples=1e6 ++seed=7301 k=1e-2
-
-python online_hierarchical.py +experiment=h-exp-large ++env_name=NRoom-v3 ++n_samples=1e6 ++seed=1001 k=1e-3
-python online_hierarchical.py +experiment=h-exp-large ++env_name=NRoom-v3 ++n_samples=1e6 ++seed=2222 k=1e-3
-python online_hierarchical.py +experiment=h-exp-large ++env_name=NRoom-v3 ++n_samples=1e6 ++seed=9181 k=1e-3
-python online_hierarchical.py +experiment=h-exp-large ++env_name=NRoom-v3 ++n_samples=1e6 ++seed=7189 k=1e-3
-python online_hierarchical.py +experiment=h-exp-large ++env_name=NRoom-v3 ++n_samples=1e6 ++seed=7301 k=1e-3
-
-python online_hierarchical.py +experiment=h-log-large ++env_name=NRoom-v3 ++n_samples=1e6 ++seed=1001 k=1e-3
-python online_hierarchical.py +experiment=h-log-large ++env_name=NRoom-v3 ++n_samples=1e6 ++seed=2222 k=1e-3
-python online_hierarchical.py +experiment=h-log-large ++env_name=NRoom-v3 ++n_samples=1e6 ++seed=9181 k=1e-3
-python online_hierarchical.py +experiment=h-log-large ++env_name=NRoom-v3 ++n_samples=1e6 ++seed=7189 k=1e-3
-python online_hierarchical.py +experiment=h-log-large ++env_name=NRoom-v3 ++n_samples=1e6 ++seed=7301 k=1e-3
