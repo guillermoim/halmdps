042ab75f0d27538d4933def5327ddb039703947c
Guillermo <guillermo.infante@upf.edu>
2024-07-16 17:37:18 +0200

    script taxi

--- run_hierarchical_taxi.sh
+++ run_hierarchical_taxi.sh
@@ -1,17 +1,16 @@
 # Taxi-v1
 
-python online_hierarchical.py +experiment=h-log-short ++env_name=Taxi-v1 ++n_samples=1e6 ++seed=1001 k=1e-1
-python online_hierarchical.py +experiment=h-log-short ++env_name=Taxi-v1 ++n_samples=1e6 ++seed=2222 k=1e-1
-python online_hierarchical.py +experiment=h-log-short ++env_name=Taxi-v1 ++n_samples=1e6 ++seed=9181 k=1e-1
-python online_hierarchical.py +experiment=h-log-short ++env_name=Taxi-v1 ++n_samples=1e6 ++seed=7189 k=1e-1
-python online_hierarchical.py +experiment=h-log-short ++env_name=Taxi-v1 ++n_samples=1e6 ++seed=7301 k=1e-1
+python online_hierarchical.py +experiment=h-exp-short ++env_name=Taxi-v1 ++n_samples=1e6 ++seed=1001
+python online_hierarchical.py +experiment=h-exp-short ++env_name=Taxi-v1 ++n_samples=1e6 ++seed=2222
+python online_hierarchical.py +experiment=h-exp-short ++env_name=Taxi-v1 ++n_samples=1e6 ++seed=9181
+python online_hierarchical.py +experiment=h-exp-short ++env_name=Taxi-v1 ++n_samples=1e6 ++seed=7189
+python online_hierarchical.py +experiment=h-exp-short ++env_name=Taxi-v1 ++n_samples=1e6 ++seed=7301
 
 # python online_hierarchical.py +experiment=h-exp-short ++env_name=Taxi-v1 ++n_samples=1e6 ++seed=1001 k=1e-1 
 # python online_hierarchical.py +experiment=h-exp-short ++env_name=Taxi-v1 ++n_samples=1e6 ++seed=2222 k=1e-1
 # python online_hierarchical.py +experiment=h-exp-short ++env_name=Taxi-v1 ++n_samples=1e6 ++seed=9181 k=1e-1
 # python online_hierarchical.py +experiment=h-exp-short ++env_name=Taxi-v1 ++n_samples=1e6 ++seed=7189 k=1e-1
 # python online_hierarchical.py +experiment=h-exp-short ++env_name=Taxi-v1 ++n_samples=1e6 ++seed=7301 k=1e-1
-
 
 # Taxi-v3
 python online_hierarchical.py +experiment=h-exp-large ++env_name=Taxi-v3 ++n_samples=2e6 ++seed=1001  ++k=1e-1
