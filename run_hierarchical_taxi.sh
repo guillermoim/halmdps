82de07dae9ca26b1bb30f03cb1b27b27e69fd8f0
Guillermo <guillermo.infante@upf.edu>
2024-08-18 14:56:46 -0700

    changed taxi script

--- run_hierarchical_taxi.sh
+++ run_hierarchical_taxi.sh
@@ -38,26 +38,26 @@ python online_hierarchical.py +experiment=h-log-large ++env_name=Taxi-v3 ++n_sam
 python online_hierarchical.py +experiment=h-log-large ++env_name=Taxi-v3 ++n_samples=2e6 ++seed=7301  
 
 # Taxi-v2
-python online_hierarchical.py +experiment=h-log-xlarge ++env_name=Taxi-v2 ++n_samples=5e6 ++seed=1001 k=1e-1
-python online_hierarchical.py +experiment=h-log-xlarge ++env_name=Taxi-v2 ++n_samples=5e6 ++seed=2222 k=1e-1
-python online_hierarchical.py +experiment=h-log-xlarge ++env_name=Taxi-v2 ++n_samples=5e6 ++seed=9181 k=1e-1
-python online_hierarchical.py +experiment=h-log-xlarge ++env_name=Taxi-v2 ++n_samples=5e6 ++seed=7189 k=1e-1
-python online_hierarchical.py +experiment=h-log-xlarge ++env_name=Taxi-v2 ++n_samples=5e6 ++seed=7301 k=1e-1
+# python online_hierarchical.py +experiment=h-log-xlarge ++env_name=Taxi-v2 ++n_samples=5e6 ++seed=1001 k=1e-1
+# python online_hierarchical.py +experiment=h-log-xlarge ++env_name=Taxi-v2 ++n_samples=5e6 ++seed=2222 k=1e-1
+# python online_hierarchical.py +experiment=h-log-xlarge ++env_name=Taxi-v2 ++n_samples=5e6 ++seed=9181 k=1e-1
+# python online_hierarchical.py +experiment=h-log-xlarge ++env_name=Taxi-v2 ++n_samples=5e6 ++seed=7189 k=1e-1
+# python online_hierarchical.py +experiment=h-log-xlarge ++env_name=Taxi-v2 ++n_samples=5e6 ++seed=7301 k=1e-1
 
-python online_hierarchical.py +experiment=h-exp-xlarge ++env_name=Taxi-v2 ++n_samples=5e6 ++seed=1001 k=1e-1
-python online_hierarchical.py +experiment=h-exp-xlarge ++env_name=Taxi-v2 ++n_samples=5e6 ++seed=2222 k=1e-1
-python online_hierarchical.py +experiment=h-exp-xlarge ++env_name=Taxi-v2 ++n_samples=5e6 ++seed=9181 k=1e-1
-python online_hierarchical.py +experiment=h-exp-xlarge ++env_name=Taxi-v2 ++n_samples=5e6 ++seed=7189 k=1e-1
-python online_hierarchical.py +experiment=h-exp-xlarge ++env_name=Taxi-v2 ++n_samples=5e6 ++seed=7301 k=1e-1
+# python online_hierarchical.py +experiment=h-exp-xlarge ++env_name=Taxi-v2 ++n_samples=5e6 ++seed=1001 k=1e-1
+# python online_hierarchical.py +experiment=h-exp-xlarge ++env_name=Taxi-v2 ++n_samples=5e6 ++seed=2222 k=1e-1
+# python online_hierarchical.py +experiment=h-exp-xlarge ++env_name=Taxi-v2 ++n_samples=5e6 ++seed=9181 k=1e-1
+# python online_hierarchical.py +experiment=h-exp-xlarge ++env_name=Taxi-v2 ++n_samples=5e6 ++seed=7189 k=1e-1
+# python online_hierarchical.py +experiment=h-exp-xlarge ++env_name=Taxi-v2 ++n_samples=5e6 ++seed=7301 k=1e-1
 
-python online_hierarchical.py +experiment=h-log-xlarge ++env_name=Taxi-v2 ++n_samples=5e6 ++seed=1001 
-python online_hierarchical.py +experiment=h-log-xlarge ++env_name=Taxi-v2 ++n_samples=5e6 ++seed=2222 
-python online_hierarchical.py +experiment=h-log-xlarge ++env_name=Taxi-v2 ++n_samples=5e6 ++seed=9181 
-python online_hierarchical.py +experiment=h-log-xlarge ++env_name=Taxi-v2 ++n_samples=5e6 ++seed=7189 
-python online_hierarchical.py +experiment=h-log-xlarge ++env_name=Taxi-v2 ++n_samples=5e6 ++seed=7301 
+# python online_hierarchical.py +experiment=h-log-xlarge ++env_name=Taxi-v2 ++n_samples=5e6 ++seed=1001 
+# python online_hierarchical.py +experiment=h-log-xlarge ++env_name=Taxi-v2 ++n_samples=5e6 ++seed=2222 
+# python online_hierarchical.py +experiment=h-log-xlarge ++env_name=Taxi-v2 ++n_samples=5e6 ++seed=9181 
+# python online_hierarchical.py +experiment=h-log-xlarge ++env_name=Taxi-v2 ++n_samples=5e6 ++seed=7189 
+# python online_hierarchical.py +experiment=h-log-xlarge ++env_name=Taxi-v2 ++n_samples=5e6 ++seed=7301 
 
-python online_hierarchical.py +experiment=h-exp-xlarge ++env_name=Taxi-v2 ++n_samples=5e6 ++seed=1001
-python online_hierarchical.py +experiment=h-exp-xlarge ++env_name=Taxi-v2 ++n_samples=5e6 ++seed=2222
-python online_hierarchical.py +experiment=h-exp-xlarge ++env_name=Taxi-v2 ++n_samples=5e6 ++seed=9181
-python online_hierarchical.py +experiment=h-exp-xlarge ++env_name=Taxi-v2 ++n_samples=5e6 ++seed=7189
-python online_hierarchical.py +experiment=h-exp-xlarge ++env_name=Taxi-v2 ++n_samples=5e6 ++seed=7301
+# python online_hierarchical.py +experiment=h-exp-xlarge ++env_name=Taxi-v2 ++n_samples=5e6 ++seed=1001
+# python online_hierarchical.py +experiment=h-exp-xlarge ++env_name=Taxi-v2 ++n_samples=5e6 ++seed=2222
+# python online_hierarchical.py +experiment=h-exp-xlarge ++env_name=Taxi-v2 ++n_samples=5e6 ++seed=9181
+# python online_hierarchical.py +experiment=h-exp-xlarge ++env_name=Taxi-v2 ++n_samples=5e6 ++seed=7189
+# python online_hierarchical.py +experiment=h-exp-xlarge ++env_name=Taxi-v2 ++n_samples=5e6 ++seed=7301
